fdaac8e8857e577df9ec27f35e106d96899d2292
DobryPpz <xaleksx960@gmail.com>
2022-11-15 18:17:34 +0100

    Drugi feature

--- Nonconf.js
+++ Nonconf.js
@@ -1,1 +1,7 @@
+const Punkt = require("./Punkt");
+
 console.log("Pierwszy feature");
+console.log("Drugi feature");
+const p1 = new Punkt(4,3.3,8);
+console.log(p1.listCoords());
+console.log(p1.dist(new Punkt(0,0,0)));
--- Punkt.js
+++ Punkt.js
@@ -4,4 +4,12 @@ class Punkt {
         this.y = y;
         this.z = z;
     }
+    listCoords(){
+        return `x:${this.x}  y:${this.y}  z:${this.z}`;
+    }
+    dist(point){
+        return Math.sqrt(Math.pow(this.x-point.x,2)+Math.pow(this.y-point.y,2)+Math.pow(this.z-point.z,2));
+    }
 }
+
+module.exports = Punkt;
