fc6fd5474b515db835e4a9d63044e77bb238cbd6
DobryPpz <xaleksx960@gmail.com>
2022-11-15 18:29:08 +0100

    10

--- Nonconf.js
+++ Nonconf.js
@@ -1,3 +1,4 @@
+const Fibo = require("./Fibo");
 const Punkt = require("./Punkt");
 
 console.log("Pierwszy feature");
@@ -5,3 +6,5 @@ console.log("Drugi feature");
 const p1 = new Punkt(4,3.3,8);
 console.log(p1.listCoords());
 console.log(p1.dist(new Punkt(0,0,0)));
+const f = new Fibo();
+console.log(f.getFib(10));
