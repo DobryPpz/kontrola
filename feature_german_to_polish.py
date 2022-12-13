8cebf4359289639a731595a49ac41176a6979b0b
wortmax <karolletsgo@gmail.com>
2022-12-13 16:57:54 +0100

    feature german to polish

---
+++ feature_german_to_polish.py
@@ -0,0 +1,16 @@
+import sys
+dict = {
+    "Winkel":"kąt",
+    "Mannschaft":"zespół",
+    "tragen":"niedźwiedź",
+    "Bücher":"książki",
+    "vergeben":"nagroga",
+    "Gulasch":"gulasz",
+    "Zähne":"zęby",
+    "handeln":"handel",
+    "Gast":"gość",
+    "zoo":"zoo",
+
+
+}
+console.log(dict[sys.argv[1]])
