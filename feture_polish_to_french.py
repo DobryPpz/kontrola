600be3fe0c716c8e330f4f77b2cc93cd5ad8f779
wortmax <karolletsgo@gmail.com>
2022-12-13 16:48:51 +0100

    feture polish to french właściwy

---
+++ feture_polish_to_french.py
@@ -0,0 +1,16 @@
+import sys
+dict = {
+    "kąt":"anlge",
+    "zespół":"équipe",
+    "niedźwiedź":"ours",
+    "książki":"livres",
+    "nagroga":"décerner",
+    "gulasz":"Goulache",
+    "zęby":"les dents",
+    "handel":"commerce",
+    "gość":"invité",
+    "zoo":"zoo",
+
+
+}
+console.log(dict[sys.argv[1]])
