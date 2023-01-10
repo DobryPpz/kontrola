af4f7bf6ef2d8f62328eb4d4f8744befb1a0a415
wortmax <karolletsgo@gmail.com>
2023-01-10 16:30:15 +0100

    plus5znowu

--- lista_lancuchow.py
+++ lista_lancuchow.py
@@ -1,4 +1,5 @@
 lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam"]
 
-posortowana_lista_lancuchow = sorted(lista_lancuchow)
-print(posortowana_lista_lancuchow)
+lista_lancuchow.sort(reverse=True)
+
+print(lista_lancuchow)
