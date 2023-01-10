afbd0ba884875a6370e9cc1533fadccca01bcb71
wortmax <karolletsgo@gmail.com>
2023-01-10 16:34:05 +0100

    odwralfaplus5znowu

--- lista_lancuchow.py
+++ lista_lancuchow.py
@@ -1,4 +1,4 @@
-lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog"]
+lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog","amd", "ryzen","master", "nvidia","intel"]
 
 lista_lancuchow.sort(reverse=True)
 
