d42b50a5698befb2b93efa1d3dde7a853a1170d7
Aleksander Modzelewski <xaleksx960@gmail.com>
2023-01-10 17:03:22 +0100

    Revert "plus5znowu"
    Błędny commit af4f7bf6ef2d8f62328eb4d4f8744befb1a0a415.

--- lista_lancuchow.py
+++ lista_lancuchow.py
@@ -1,5 +1,4 @@
 lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog","amd", "ryzen","master", "nvidia","intel"]
 
-lista_lancuchow.sort(reverse=True)
-
-print(lista_lancuchow)
+posortowana_lista_lancuchow = sorted(lista_lancuchow)
+print(posortowana_lista_lancuchow)
