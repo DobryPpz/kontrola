079cc5582f188b72471049d73794fc2d645aac7f
wortmax <karolletsgo@gmail.com>
2023-01-10 22:34:44 +0100

    sadf

--- lista_lancuchow_feture.py
+++ lista_lancuchow_feture.py
@@ -1,6 +1,6 @@
 
 
-lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog","amd", "ryzen","master", "nvidia","intel","system","kontroli","wersji","attack","on"]
+lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog","amd", "ryzen","master", "nvidia","intel","system","kontroli","wersji","attack","on","titan", "ost","zero", "eclipse","theme"]
 
 posortowana_lista_lancuchow = sorted(lista_lancuchow)
 print(posortowana_lista_lancuchow)
