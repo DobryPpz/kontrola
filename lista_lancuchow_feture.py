d7d97a4589185a4995d2b6174fcd5654c3abd08d
wortmax <karolletsgo@gmail.com>
2023-01-10 22:31:07 +0100

    sad

---
+++ lista_lancuchow_feture.py
@@ -0,0 +1,6 @@
+
+
+lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki", ".", "dwa","pies", "komin","steam", "trzy", "origin","ubisoft", "connect","gog","amd", "ryzen","master", "nvidia","intel","system","kontroli","wersji","attack","on"]
+
+posortowana_lista_lancuchow = sorted(lista_lancuchow)
+print(posortowana_lista_lancuchow)
