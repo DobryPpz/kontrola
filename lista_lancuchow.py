2ab3ac37f170057a227f53c81d2d4e855d291efe
wortmax <karolletsgo@gmail.com>
2023-01-10 16:27:18 +0100

    plus5

--- lista_lancuchow.py
+++ lista_lancuchow.py
@@ -1,4 +1,4 @@
-lista_lancuchow = ["Ala", "ma", "kota", "a", "kot"]
+lista_lancuchow = ["Ala", "ma", "kota", "a", "kot", "ma", "jablko", "i", "dwa","ogurki"]
 
 posortowana_lista_lancuchow = sorted(lista_lancuchow)
 print(posortowana_lista_lancuchow)
